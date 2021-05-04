05df072363b7cc81a7ba284e44d3cf1ef82deb75
arthurvncs <arthurxd.vncs@gmail.com>
2021-05-04 13:34:26 -0300

    Adicionando django-adminlte3 e banco de dados postgreSQL

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,4 @@
 """fan2021 URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
@@ -16,8 +15,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from vendas import urls as urls_vendas
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('vendas/', include(urls_vendas)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+              + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
